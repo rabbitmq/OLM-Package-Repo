817dd7d9169aaf927de7511386c7edf738d9ef53
Daniele Palaia <dpalaia@vmware.com>
2022-07-28 10:42:18 +0200

    fixing repo image

--- generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
+++ generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
@@ -44,11 +44,11 @@ os.system("ytt -f ./overlays/overlay-deployment.yaml -f ./tmpmanifests/cluster-s
 # Create the bundle structure
 rabbitmq_cluster_operator_dir=output_directory+"/" + version 
 rabbitmq_cluster_operator_dir_manifests=rabbitmq_cluster_operator_dir+"/manifests"
-replace_rabbitmq_cluster_operator_image("./tmpmanifests/rabbitmq.clusterserviceversion.yaml","rabbitmqoperator/cluster-operator:"+version, "dockerhub.io/rabbitmqoperator/cluster-operator:"+version)
+replace_rabbitmq_cluster_operator_image("./tmpmanifests/rabbitmq.clusterserviceversion.yaml","rabbitmqoperator/cluster-operator:"+version, "docker.io/rabbitmqoperator/cluster-operator:"+version)
 os.system("mkdir -p " + rabbitmq_cluster_operator_dir_manifests)
 os.system("cp ./tmpmanifests/rabbitmq.clusterserviceversion.yaml " + rabbitmq_cluster_operator_dir_manifests)
 os.system("cat ./generators/license.yaml >" + rabbitmq_cluster_operator_dir_manifests+"/crds.yaml")
-os.system("tail -n +9 ./manifests_crds/crds.yaml >>" + rabbitmq_cluster_operator_dir_manifests+"/crds.yaml")
+os.system("tail -n +8 ./manifests_crds/crds.yaml >>" + rabbitmq_cluster_operator_dir_manifests+"/crds.yaml")
 
 # Cleanup
 os.system("rm ./tmpmanifests/*")
