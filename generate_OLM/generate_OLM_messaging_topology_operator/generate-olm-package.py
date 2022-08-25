c386ae57e80c8202786a9ab2ffe18f2c8f440e6f
Daniele Palaia <dpalaia@vmware.com>
2022-08-25 13:58:56 +0200

    managing replace field on messaging topology operator

--- generate_OLM/generate_OLM_messaging_topology_operator/generate-olm-package.py
+++ generate_OLM/generate_OLM_messaging_topology_operator/generate-olm-package.py
@@ -17,8 +17,16 @@ operator_release_file = sys.argv[1]
 version = sys.argv[2]
 output_directory = sys.argv[3]
 
+# Manage update field to update versioning
+f = open("./generators/replace.txt")
+replaces = f.readline().strip()
+oldversion= f.readline().strip()
+f.close()
+
+replace_rabbitmq_cluster_operator_image("./generators/cluster-service-version-generator.yml","rabbitmqoperator/messaging-topology-operator:"+oldversion, "rabbitmqoperator/messaging-topology-operator:"+version)
+
 # Apply version to the service-version generator
-ytt_command_add_version = "ytt -f ./generators/cluster-service-version-generator.yml --data-value-yaml name=rabbitmq-messaging-topology-operator.v"+version+" --data-value-yaml version="+version+ "> ./tmpmanifests/cluster-service-version-generator.yaml"
+ytt_command_add_version = "ytt -f ./generators/cluster-service-version-generator.yml --data-value-yaml name=rabbitmq-messaging-topology-operator.v"+version+" --data-value-yaml version="+version+ " --data-value-yaml replaces="+replaces+ "> ./tmpmanifests/cluster-service-version-generator.yaml"
 os.system(ytt_command_add_version)
 
 # Create a copy of the cluster operator and add --- at the end of the file
