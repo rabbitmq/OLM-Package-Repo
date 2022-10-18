e8e14ba0654441d8482d7edf8035a71d8806dff7
Daniele Palaia <dpalaia@vmware.com>
2022-10-18 16:52:23 +0200

    improving version management in cluster operator

--- generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
+++ generate_OLM/generate_OLM_cluster_operator/generate-olm-package.py
@@ -21,8 +21,8 @@ replaces = f.readline().strip()
 oldversion= f.readline().strip()
 f.close()
 
-replace_rabbitmq_cluster_operator_image("./generators/cluster-service-version-generator.yml","rabbitmqoperator/cluster-operator:"+oldversion, "rabbitmqoperator/cluster-operator:"+version)
-replace_rabbitmq_cluster_operator_image("./generators/cluster-service-version-generator-openshift.yml","rabbitmqoperator/cluster-operator:"+oldversion, "rabbitmqoperator/cluster-operator:"+version)
+replace_rabbitmq_cluster_operator_image("./generators/cluster-service-version-generator.yml","rabbitmqoperator/cluster-operator:0.0.0", "rabbitmqoperator/cluster-operator:"+version)
+replace_rabbitmq_cluster_operator_image("./generators/cluster-service-version-generator-openshift.yml","rabbitmqoperator/cluster-operator:0.0.0", "rabbitmqoperator/cluster-operator:"+version)
 # Apply version to the service-version generator
 ytt_command_add_version = "ytt -f ./generators/cluster-service-version-generator.yml --data-value-yaml name=rabbitmq-cluster-operator.v"+version+" --data-value-yaml version="+version+ " --data-value-yaml replaces="+replaces+ "> ./tmpmanifests/cluster-service-version-generator.yaml"
 os.system(ytt_command_add_version)
