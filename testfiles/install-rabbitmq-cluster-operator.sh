2cd65ad65ffbac1ce2aec7eb3e0b7cfcfaffd533
Daniele Palaia <dpalaia@vmware.com>
2024-02-25 16:13:08 +0100

    managing package versions

--- testfiles/install-rabbitmq-cluster-operator.sh
+++ testfiles/install-rabbitmq-cluster-operator.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+VERSION=${1:-"0.0.0"}
+
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
+
 kubectl create -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
 kubectl create ns rabbitmq-system-olm
 kubectl create -f $PWD/OLM-Package-Repo/testfiles/og.yaml
--- testfiles/uninstall-rabbitmq-cluster-operator.sh
+++ testfiles/uninstall-rabbitmq-cluster-operator.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-kubectl delete csv rabbitmq-cluster-operator.v0.0.0 -n rabbitmq-system-olm
+VERSION=${1:-"0.0.0"}
+
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
+
+kubectl delete csv rabbitmq-cluster-operator.v"$VERSION" -n rabbitmq-system-olm
 kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
 kubectl delete -f $PWD/OLM-Package-Repo/testfiles/og.yaml
 kubectl delete ns rabbitmq-system-olm
