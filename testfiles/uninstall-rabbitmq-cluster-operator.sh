a0f337dad8aa321e8aebb56dd6251e7893ae14be
Daniele Palaia <dpalaia@vmware.com>
2024-02-28 10:43:09 +0100

    messaging topology operator new scripts

--- testfiles/uninstall-rabbitmq-cluster-operator.sh
+++ testfiles/uninstall-rabbitmq-cluster-operator.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 VERSION=${1:-"0.0.0"}
 
-sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
-sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
+sed -i -e "s/0.0.0/"$VERSION"/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
+sed -i -e "s/0.0.0/"$VERSION"/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
 
 kubectl delete csv rabbitmq-cluster-operator.v"$VERSION" -n rabbitmq-system-olm
 kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
---
+++ testfiles/uninstall-rabbitmq-messaging-topology-operator.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+VERSION=${1:-"0.0.0"}
+
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-messaging-operator-catalog.yaml
+sed -i -e "s/0.0.0/$VERSION/g" $PWD/OLM-Package-Repo/testfiles/rabbitmq-messaging-operator-subscription.yaml
+
+kubectl delete csv rabbitmq-cluster-operator.v0.0.0 -n rabbitmq-system-olm
+kubectl delete csv rabbitmq-messaging-topology-operator.v"$VERSION" -n rabbitmq-system-olm
+kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-operator-subscription.yaml
+kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-messaging-operator-subscription.yaml
+kubectl delete -f $PWD/OLM-Package-Repo/testfiles/og.yaml
+kubectl delete ns rabbitmq-system-olm
+kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-cluster-operator-catalog.yaml
+kubectl delete -f $PWD/OLM-Package-Repo/testfiles/rabbitmq-messaging-operator-catalog.yaml
