11b378bb2814ecc0546e3acd8cfbb1751b5bce52
Daniele Palaia <dpalaia@vmware.com>
2024-05-07 19:23:44 +0200

    fixig an issue for replace field

--- rabbitmq_olm_package_repo/utils.py
+++ rabbitmq_olm_package_repo/utils.py
@@ -12,17 +12,14 @@ class OperatorType(Enum):
 def create_overlay(
     release_file, kind, firstString, endString, file_generator, file_output
 ):
-
     found = False
     parsing = False
 
     with open(release_file, "r") as myfile:
-
         filestring = ""
 
         for line in myfile:
             if parsing == True:
-
                 if line.find(endString) >= 0:
                     if found == True:
                         os.system("cp " + str(file_generator) + " " + str(file_output))
@@ -45,21 +42,18 @@ def create_overlay(
 
 # This function complete an overlay generator file (in ./generators) for Role, Clusterrole and Deployment
 def replace_rabbitmq_cluster_operator_version_overlay(file_input, pattern1, pattern2):
-
     with fileinput.FileInput(file_input, inplace=True, backup=".bak") as file:
         for line in file:
             print(line.replace(pattern1, pattern2), end="")
 
 
 def replace_rabbitmq_cluster_operator_image(file_input, pattern1, pattern2):
-
     with fileinput.FileInput(file_input, inplace=True, backup=".bak") as file:
         for line in file:
             print(line.replace(pattern1, pattern2), end="")
 
 
 def replace_if_rabbitmq_webhook(file_input):
-
     with fileinput.FileInput(file_input, inplace=True, backup=".bak") as file:
         for line in file:
             print(
@@ -71,7 +65,6 @@ def replace_if_rabbitmq_webhook(file_input):
 
 
 def get_operator_name(file_input):
-
     with open(file_input) as f:
         if "rabbitmqoperator/messaging-topology-operator" in f.read():
             return OperatorType.MESSAGING_TOPOLOGY_OPERATOR
@@ -80,7 +73,6 @@ def get_operator_name(file_input):
 
 
 def get_operator_last_tag(operator):
-
     last_tag_file = "./last_tag_file"
     os.system(
         "curl https://api.github.com/repos/rabbitmq/"
